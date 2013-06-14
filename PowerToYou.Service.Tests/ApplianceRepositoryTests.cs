af9d25227ba3a908cb3c9e2009260d28cd724be1
Bala <bala.iyer1973@gmail.com>
2013-06-14 22:25:07 +0100

    First commit

---
+++ PowerToYou.Service.Tests/ApplianceRepositoryTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using PowerToYou.Service.Appliance;
+
+namespace PowerToYou.Service.Tests
+{
+    [TestFixture]
+    public class ApplianceRepositoryTests
+    {
+        private ApplianceRepository _applianceRepository;
+
+        [SetUp]
+        public void Setup()
+        {
+            _applianceRepository = new ApplianceRepository(@"C:\Users\Admin\.ssh\PowerToYou\AppChallenge\appliances.xml");
+        }
+
+        [Test]
+        public void GetAppliances_ReturnsAllAppliancesInTheXMLFile()
+        {
+            var appliances = _applianceRepository.GetAppliances();
+            Assert.IsTrue(appliances.Count() == 27);
+        }
+    }
+}
---
+++ Service/Appliance/ApplianceRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace PowerToYou.Service.Appliance
+{
+    public class ApplianceRepository : IApplianceRepository
+    {
+        private readonly string _fileName;
+
+        public ApplianceRepository(string fileName)
+        {
+            _fileName = fileName;
+        }
+
+        public IEnumerable<Appliance> GetAppliances()
+        {
+            var doc = XElement.Load(_fileName);
+
+            return doc.Descendants("appliance")
+               .Select(appliance => new Service.Appliance.Appliance
+                {
+                    Name = appliance.Attribute("name").Value,
+                    Wattage = Convert.ToDouble(appliance.Attribute("wattage").Value)
+                });
+        }
+    }
+}
